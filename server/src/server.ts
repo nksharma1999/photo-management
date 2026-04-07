90d46862a192c8ae49be9e9a89a456c817b12397
Nandkishor Sharma <38472@suzlon.com>
2026-04-07 15:11:48 +0530

    Image issues resolve

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,16 +11,18 @@ import albumRoutes from "./routes/albumRoutes";
 dotenv.config({path:"./config/.env"});
 const PORT = process.env.SERVER_PORT
 const MONGODB_URL = process.env.MONGODB_URL;
-
+const facePath = process.env.FACES_PATH || "Faces";
+const uploadsPath = process.env.UPLOADS_PATH || "uploads";
+const thumbnailsPath = process.env.THUMBNAILS_PATH || "thumbnails"
 const app = express()
 connectDB(MONGODB_URL);
 
 app.use(cors())
 app.use(express.json())
 
-app.use("/Faces", express.static("Faces"))
-app.use("/uploads", express.static("uploads"))
-app.use("/thumbnails", express.static("thumbnails"))
+app.use("/Faces", express.static(facePath))
+app.use("/uploads", express.static(uploadsPath))
+app.use("/thumbnails", express.static(thumbnailsPath))
 
 app.get("/api/server-health", (req,res) =>{
     res.status(200).json("Server Running...");
